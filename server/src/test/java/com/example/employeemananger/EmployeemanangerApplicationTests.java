e0ce797b8c3c9ab1049d52a0884f79e3d4a8c175
hunggoodkidz <ninbook0708@gmail.com>
2023-04-24 14:10:49 +0700

    finishes the course training

---
+++ server/src/test/java/com/example/employeemananger/EmployeemanangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemananger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
